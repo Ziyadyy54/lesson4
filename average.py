5efdbc9f28f7f8f064f1c8f8591cbf9f6ea898ec
ziyad <mziyadbsk@gmail.com>
2024-08-31 20:19:05 +0300

    lesson 4

---
+++ average.py
@@ -0,0 +1,9 @@
+letterA =328
+letterB =654
+letterC =890
+letterD =-228
+totalvalue=(letterA+letterB+letterC+letterD)
+print("total value is ",totalvalue)
+
+average=(totalvalue/4)
+print("the average is",average)
---
+++ percentage.py
@@ -0,0 +1,8 @@
+math =int(input("enter your marks of math"))
+topic =int(input("enter your marks of topic/science"))
+english =int(input("enter your english marks"))
+coding =int(input("enter your computing/coding marks"))
+total=(math+topic+english+coding)
+print("your total grade was ",total)
+percentage=(total/400)*100
+print("your percentage is",percentage)
